d1e1275ed368a22680a25f3c1bdf34f66cc8dd75
1214wuai <1138963990@qq.com>
2019-06-15 19:43:35 +0800

    字符串分割回文完全递推版

---
+++ 字符串分割成回文完全动规版/字符串分割成回文完全动规版/源.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+vector<vector<bool>> is_PalDP(string s)
+{
+	int n = s.size();
+	vector<vector<bool>> is_pal(n, vector<bool>(n, false));
+	for (int i = n - 1; i >= 0; i--)
+	{
+		for (int j = i; j < n; j++)
+		{
+			if (j == i)
+				is_pal[i][j] = true;//F[i][i] = true   初始化
+			else if (j == i + 1)//连续两个字符
+				is_pal[i][j] = (s[i] == s[j]);
+			else  //j>i+1
+				is_pal[i][j] = (s[i] == s[j] && is_pal[i + 1][j - 1]);
+		}
+	}
+	return is_pal;
+}
+int MinOfCut(string & s)
+{
+	if (s.empty())
+		return -1;
+	int n = s.size();
+	vector<int> cut(n + 1, 0);
+	//初始化  F[i] = i-1
+	for (int i = 0; i < n + 1; i++)
+		cut[i] = i - 1;
+	vector<vector<bool>> is_pal = is_PalDP(s);
+	//递推
+	for (int i = 1; i < n + 1; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			//j<i
+			//substr[j+1,i)是回文串
+			if (is_pal[j][i - 1])
+			{
+				//F(i) = min(F(j)+1,F(i))
+				cut[i] = min(cut[j] + 1, cut[i]);
+			}
+		}
+	}
+	return cut[n];
+}
+int main()
+{
+	string s("aaabaa");
+	cout << MinOfCut(s) << endl;
+	system("pause");
+	return 0;
+}
