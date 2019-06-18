b8fb1b0b3bc90e6cc513e3e495e806e526074b44
1214wuai <1138963990@qq.com>
2019-06-18 17:04:29 +0800

    机器人从网格左上角走到右下角，路径总数

---
+++ Project1/Project1/源.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int uniquePaths(int m, int n)
+{
+	if (m < 1 || n < 1)
+		return 0;
+	//³õÊ¼»¯  F(0,i) = F(i,0) = 1;
+	vector<vector<int>> path(m, vector<int>(n, 1));
+	//µÝÍÆ
+	for (int i = 1; i < m; i++)
+	{
+		for (int j = 1; j < n; j++)
+		{
+			path[i][j] = path[i - 1][j] + path[i][j - 1];
+		}
+	}
+	return path[m - 1][n - 1];
+}
+int main()
+{
+	cout << uniquePaths(3,3) << endl;
+	system("pause");
+	return 0;
+}
