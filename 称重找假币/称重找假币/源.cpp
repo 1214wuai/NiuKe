c2e4dc9ce195bab708c70d1b4eb716f50ece5ea3
1214wuai <1138963990@qq.com>
2019-07-21 11:08:09 +0800

    称重找假币，分三份

--- 称重找假币/称重找假币/源.cpp
+++ 称重找假币/称重找假币/源.cpp
@@ -8,15 +8,25 @@ int main()
 	int n;
 	while (cin >> n)
 	{
-		if (n == 0)
-			return 0;
+		//if (n == 0)
+		//	return 0;
+		//int count = 0;
+		//while (n>1)
+		//{
+		//	n /= 2;
+		//	count++;
+		//}
+
+		//cout << count << endl;
 		int count = 0;
-		while (n>1)
+		while (n >= 2)
 		{
-			n /= 2;
+			if (n % 3)
+				n = n / 3 + 1;
+			else
+				n = n / 3;
 			count++;
 		}
-
 		cout << count << endl;
 	}
 }
