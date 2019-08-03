3771c3e6fad941dc2a8bbe38bb182940a0b0be08
1214wuai <1138963990@qq.com>
2019-08-03 17:11:17 +0800

    递归

--- 字符串分割成回文/字符串分割成回文/源.cpp
+++ 字符串分割成回文/字符串分割成回文/源.cpp
@@ -1,31 +1,90 @@
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
+//
+//bool isPal(string s, int i, int j)
+//{
+//	while (i < j)
+//	{
+//		if (s[i] != s[j])
+//			return false;
+//		i++;
+//		j--;
+//	}
+//	return true;
+//}
+////动态规划：判断一个字符区间的子串是否为回文串
+//
+//int MinOfCut(string & s)
+//{
+//	if (s.empty())
+//		return -1;
+//	int n = s.size();
+//	vector<int> cut(n+1,0);
+//	//初始化  F[i] = i-1
+//	for (int i = 0; i < n + 1; i++)
+//		cut[i] = i - 1;
+//	//递推
+//	for (int i = 1; i < n + 1; i++)
+//	{
+//		for (int j = 0; j < i; j++)
+//		{
+//			//j<i
+//			//substr[j+1,i)是回文串
+//			if (isPal(s, j, i - 1))
+//			{
+//				//F(i) = min(F(j)+1,F(i))
+//				cut[i] = min(cut[j] + 1, cut[i]);
+//			}
+//		}
+//	}
+//	return cut[n];
+//
+//}
+//int main()
+//{
+//	string s("ab");
+//	cout << MinOfCut(s) << endl;
+//	system("pause");
+//	return 0;
+//}
 #include<iostream>
 #include<string>
 #include<vector>
 #include<algorithm>
+
 using namespace std;
 
-bool isPal(string s, int i, int j)
+vector<vector<bool>> is_PalDP(string s)
 {
-	while (i < j)
+	int n = s.size();
+	vector<vector<bool>> is_pal(n, vector<bool>(n, false));
+	for (int i = n - 1; i >= 0; i--)
 	{
-		if (s[i] != s[j])
-			return false;
-		i++;
-		j--;
+		for (int j = i; j < n; j++)
+		{
+			if (j == i)
+				is_pal[i][j] = true;//F[i][i] = true   初始化
+			else if (j == i + 1)//连续两个字符
+				is_pal[i][j] = (s[i] == s[j]);
+			else  //j>i+1
+				is_pal[i][j] = (s[i] == s[j] && is_pal[i + 1][j - 1]);
+		}
 	}
-	return true;
+	return is_pal;
 }
-//动态规划：判断一个字符区间的子串是否为回文串
-
 int MinOfCut(string & s)
 {
 	if (s.empty())
 		return -1;
 	int n = s.size();
-	vector<int> cut(n+1,0);
+	vector<int> cut(n + 1, 0);
 	//初始化  F[i] = i-1
 	for (int i = 0; i < n + 1; i++)
 		cut[i] = i - 1;
+	vector<vector<bool>> is_pal = is_PalDP(s);
 	//递推
 	for (int i = 1; i < n + 1; i++)
 	{
@@ -33,7 +92,7 @@ int MinOfCut(string & s)
 		{
 			//j<i
 			//substr[j+1,i)是回文串
-			if (isPal(s, j, i - 1))
+			if (is_pal[j][i - 1])
 			{
 				//F(i) = min(F(j)+1,F(i))
 				cut[i] = min(cut[j] + 1, cut[i]);
@@ -41,7 +100,6 @@ int MinOfCut(string & s)
 		}
 	}
 	return cut[n];
-
 }
 int main()
 {
